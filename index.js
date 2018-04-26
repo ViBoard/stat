d3412e876c548470c001d716bd83444fa2924342
salamantos <m.b.volkov@yandex.ru>
2018-04-26 14:48:50 +0300

    add iframe

--- index.js
+++ index.js
@@ -4,23 +4,32 @@ var query = {
 	limit: 100
 }
 
-var count = 0
-var sum = 0
+var count_yout = 0
+var sum_yout = 0
+var count_iframe = 0
+var sum_iframe = 0
 
 function getCount () {
 	var k = 0;
+	var t = 0;
 	golos.api.getDiscussionsByCreated(query, function(err, result) {
 		for (var i = 0; i < 100; ++i) {
-			try {	
+			try {
 				if (result[i].body.includes("yout")) {
 					// console.log(result[i])
 					++k;
 				}
+				if (result[i].body.includes("iframe")) {
+					// console.log(result[i])
+					++t;
+				}
 			} catch (e) {}
 		}
-		sum += k
-		count++
-		console.log("Last:", k, "Average:", sum/count, "Count:", count)
+		sum_yout += k
+		count_yout++
+		sum_iframe += t
+		count_iframe++
+		console.log("'yout/iframe':", k, t, "Avg:", sum_yout/count_yout, sum_iframe/count_iframe, "Count:", count_yout, count_iframe)
 	});
 
 }
